2cd9787a2f55fd2551ae12141a47299e91e2352f
Sam Zagrobelny <strangesast@gmail.com>
2019-07-02 14:06:51 -0400

    moving

--- arduino/sketch.ino
+++ arduino/sketch.ino
@@ -2,11 +2,17 @@
 #include <SoftwareSerial.h>
 #include <Tiny4kOLED.h>
 
+
+// both temp sensors connected to attiny85 pin 6 (PB1)
 #define ONE_WIRE_BUS 1
+// pin 2 (PB3)
 #define RxD 3
+// pin 3 (PB4)
 #define TxD 4
-#define EBADTEMP -999
-#define BROADCAST_NAME "tempSensor"
+#define STARTCONVO 0x44
+#define READTEMP 0xBE
+#define BLUETOOTH_NAME "tempSensor"
+
 
 OneWire oneWire(ONE_WIRE_BUS);
 SoftwareSerial serial(RxD, TxD);
@@ -18,42 +24,47 @@ float temp;
 
 
 void setup() {
-  delay(1000);
+  delay(1000); // wait for oled to get ready?
+
+  // oled setup
   oled.begin();
   oled.setFont(FONT8X16);
   oled.clear();
   oled.switchRenderFrame();
   oled.on();
 
+  // hm10 bluetooth setup
   serial.begin(9600);
-  serial.print("AT+NAME" + BROADCAST_NAME);
+  serial.print("AT+NAME" + BLUETOOTH_NAME);
 }
 
 void loop() {
-  // request temperatures
+  // ask all sensors for temps
   oneWire.reset();
   oneWire.skip();
-  oneWire.write(0x44, 0); // start convo, no parasite
+  oneWire.write(STARTCONVO, 0); // start convo, no parasite
 
-  // allow temps to be calculated
-  delay(750); // probably too much time
-  oled.clear(); // temps in same position, probabaly unnecessary
-
-  oneWire.reset_search();
+  // wait for calculation
+  // could alternatively check for value by probing read_bit()
+  //unsigned long now = millis();
+  // calcMS = {9: 94, 10: 188, 11: 375, default: 750};
+  //while (oneWire.read_bit() != 1 && (millis() - calcMS < now));
+  delay(750);
+  oled.clear(); // clear the buffer (not displayed, yet)
 
   int i = 0;
-  int offset = 0;
-  // addr value updated by search
+  int msgPos = 0;
+  oneWire.reset_search();
   while (oneWire.search(addr)) {
-    // is this a valid address?
+    // validate address
     if (OneWire::crc8(addr, 7) == addr[7]) {
-      oled.setCursor(64 * i, 2);
       oneWire.reset();
       oneWire.select(addr);
-      oneWire.write(0xBE);
+      oneWire.write(READTEMP);  // read buf
       for (uint8_t k = 0; k < 9; k++) {
         data[k] = oneWire.read();
       }
+      // validate response
       if (OneWire::crc8(data, 8) == data[8]) {
         temp16 = (((int16_t) data[1]) << 11) | (((int16_t) data[0]) << 3);
         // temp = (float) temp16 * 0.0078125; // c
@@ -63,20 +74,23 @@ void loop() {
         temp = 0;
       }
       for (int j = 2; j < 8; j++) {
-        msg[offset++] = addr[j];
+        msg[msgPos++] = addr[j];
       }
-      msg[offset++] = (byte) temp16 >> 8;
-      msg[offset++] = (byte) temp16;
-
-      offset += 2;
+      msg[msgPos++] = (byte) (temp16 >> 8);
+      msg[msgPos++] = (byte) temp16;
+      
+      oled.setCursor(64 * i, 2);
       oled.print(temp);
+      
+      msgPos += 2;
       i++;
     }
   }
-  // service characteristic value keeps last 20 bytes
   for (byte j = 0; j < 20; j++) {
     serial.write(msg[j]);
   }
-  oled.switchFrame(); // swap display buffers
+  oled.switchFrame(); // render temps
+
+  // could alternatively tell chip to sleep
   delay(1000);
 }
