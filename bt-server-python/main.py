c1538f680aba3a927da1427517cf38c272d354f8
Sam Zagrobelny <strangesast@gmail.com>
2019-08-29 14:02:51 -0400

    partial

--- bt-server-python/main.py
+++ bt-server-python/main.py
@@ -1,46 +1,49 @@
-import grpc
 import os
-from google.protobuf.timestamp_pb2 import Timestamp
 import random
 import time
+import grpc
+from google.protobuf.timestamp_pb2 import Timestamp
 
 import pooltempsensor_pb2
 import pooltempsensor_pb2_grpc
 
 
 def now():
-    now = time.time()
-    seconds = int(now)
-    nanos = int((now - seconds) * 10**9)
+    """return protobuf timestamp"""
+    current_time = time.time()
+    seconds = int(current_time)
+    nanos = int((current_time - seconds) * 10**9)
     timestamp = Timestamp(seconds=seconds, nanos=nanos)
     return timestamp
 
 
 def test():
-    for each in range(10):
+    """return ten sample data points"""
+    for _ in range(10):
         date = now()
-        temps = pooltempsensor_pb2.Temps(values={
-            '000000': random.random(),
-            '000001': random.random(),
-            }, date=date)
+        temps = pooltempsensor_pb2.Temps(
+            values={
+                '000000': random.random(),
+                '000001': random.random(),
+            },
+            date=date
+        )
         yield temps
 
 
 
 if __name__ == '__main__':
-    host = os.environ.get('GRPC_HOST') or 'localhost'
-    port = os.environ.get('GRPC_PORT') or '50051'
-    
-    
-    iterator = test()
-    time.sleep(2)
+    HOST = os.environ.get('GRPC_HOST') or 'localhost'
+    PORT = os.environ.get('GRPC_PORT') or '50051'
 
-    with grpc.insecure_channel('{}:{}'.format(host, port)) as channel:
-    
-    
+    TEST_ITERATOR = test()
+
+    with grpc.insecure_channel('{}:{}'.format(HOST, PORT)) as channel:
         stub = pooltempsensor_pb2_grpc.TempSensorStub(channel)
-        response = stub.RecordTemps(iterator)
+        response = stub.RecordTemps(TEST_ITERATOR)
+
         if response is None:
             print('no response')
         else:
-            print('Recorded {} values from {} new sensors'.format(response.count, response.sensorCount))
+            print('Recorded {} values from {} new sensors'.format(
+                response.count, response.sensorCount))
